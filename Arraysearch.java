735852a4f46b814e1cc9180ae68a76e055230826
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-16 13:09:47 +0300

    Modify arrays search

--- Arraysearch.java
+++ Arraysearch.java
@@ -3,14 +3,14 @@ public class Arraysearch{
         int a[]={5,4,3,2,1};
         boolean found=false;
         for(int i=0; i<a.length; i++){
-            if(a[i]==9){
-                System.out.println(i);
+            if(a[i]==4){
+                System.out.println("Element found at index "+i);
                 found=true;
                 break;
             }
         }
         if(found==false){
-            System.out.println("Not in array");
+            System.out.println("Element not in array");
         }
         
     }
