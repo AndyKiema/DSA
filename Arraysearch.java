9cb7dcdea3ed7045a0c7ab54252060bed06a6a2b
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-16 11:29:15 +0300

    Add array search file

--- Arraysearch.java
+++ Arraysearch.java
@@ -1,11 +1,17 @@
 public class Arraysearch{
     public static void main(String[]args){
         int a[]={5,4,3,2,1};
+        boolean found=false;
         for(int i=0; i<a.length; i++){
-            if(a[i]==3){
+            if(a[i]==9){
                 System.out.println(i);
+                found=true;
+                break;
             }
         }
-        System.out.println("Not in array");
+        if(found==false){
+            System.out.println("Not in array");
+        }
+        
     }
 }
